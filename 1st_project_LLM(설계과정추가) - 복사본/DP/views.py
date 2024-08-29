4a383cb3fd5e1bb06d7c69389c076be14442dd13
saab9699-minsu <saab96994@gmail.com>
2024-08-29 09:24:49 +0900

    Delete 1st_project_LLM(설계과정추가) - 복사본 directory

--- 1st_project_LLM(설계과정추가) - 복사본/DP/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render, redirect
-
-#3
-def index(request):
-    if request.user.is_authenticated:
-        return redirect("cafe:main")
-    else:
-        return redirect("users:login")
--- 1st_project_LLM(설계과정추가) - 복사본/README.md
+++
@@ -1,1 +0,0 @@
-# 1st_project_LLM
